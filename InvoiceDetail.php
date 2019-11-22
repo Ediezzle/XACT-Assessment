a647cd501d380ce2a4a0458805d9bb892847624f
Ediezzle <edmore.mandikiyana@gmail.com>
2019-11-22 16:10:30 +0200

    linked files with StockMaster

--- InvoiceDetail.php
+++ InvoiceDetail.php
@@ -16,26 +16,27 @@
             <h3 class="pageCenter"> <b>Invoice Detail</b></h3>
             <br>
             <div class="row">
-                <div class="col-lg-2"> <strong>Date</strong></div>
+                <div class="col-lg-3"> <strong>Date</strong></div>
                 <div class="col-lg-1"> <strong>Invoice Number</strong></div>
                 <div class="col-lg-1"> <strong>Account Code</strong></div>
-                <div class="col-lg-2"> <strong>Name & Surname</strong></div>
+                <div class="col-lg-1"> <strong>Name & Surname</strong></div>
                 <div class="col-lg-1"> <strong>Item Number</strong></div>
                 <div class="col-lg-1"> <strong>Stock Code</strong></div>
                 <div class="col-lg-1"> <strong>Quantity Sold</strong></div>
                 <div class="col-lg-1"> <strong>Unit Cost</strong></div>
                 <div class="col-lg-1"> <strong>Unit Sell</strong></div>
                 <div class="col-lg-1 "> <strong>Discount</strong></div>
+				
 
             </div>
             <div class="row">
-                <div class="col-lg-2"><input type="date" name="date" id="date" class="form-control" required />
+                <div class="col-lg-3"><input type="date" name="date" id="date" class="form-control" required />
                 </div>
-                <div class="col-lg-1"><input type="text" name="invoiceNumber" id="invoiceNumber" class="form-control" disabled value="AUTO"/>
+                <div class="col-lg-1"><input type="text" name="invoiceNumber" id="invoiceNumber" class="form-control" readonly value="AUTO"/>
                 </div>
                 <div class="col-lg-1"><input type="text" name="accCode" id="accCode" class="form-control" required />
                 </div>
-                <div class="col-lg-2"><input type="text" name="name" id="name" class="form-control" required />
+                <div class="col-lg-1"><input type="text" name="name" id="name" class="form-control" required />
                 </div>
                 <div class="col-lg-1"><input type="text" name="itemNumber" id="itemNumber" class="form-control" required />
                 </div>
@@ -43,12 +44,13 @@
                 </div>
                 <div class="col-lg-1"><input type="text" name="quantitySold" id="quantitySold" class="form-control" required />
                 </div>
-                <div class="col-lg-1"><input type="text" name="unitCost" id="unitCost" class="form-control" required />
+                <div class="col-lg-1"><input type="text" name="unitCost" id="unitCost" class="form-control" readonly value="Auto"/>
                 </div>
-                <div class="col-lg-1"><input type="text" name="unitSell" id="unitSell" class="form-control" required />
+                <div class="col-lg-1"><input type="text" name="unitSell" id="unitSell" class="form-control" readonly value="Auto"/>
                 </div>
-                <div class="col-lg-1"><input type="text" name="discount" id="discount" class="form-control" required />
+                <div class="col-lg-1"><input type="text" name="discount" id="discount" class="form-control" required/>
                 </div>
+				
 
             </div>
             <br />
@@ -81,17 +83,11 @@
         </div>
     </form>
 
-
-
-
-
-
-
     <script src="bootstrap/js/jquery.js" type="text/javascript"></script>
     <script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
     <script>
-        function displayTotal() {
-            document.getElementById("subTotal").innerHTML
+        function calcDiscount() {
+            document.getElementById("discount").innerHTML=8;
         }
     </script>
     <script>
@@ -99,7 +95,8 @@
             window.location.reload();
         }
     </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+   
 </body>
 
 </html>
--- Invoice_Detail_Input.php
+++ Invoice_Detail_Input.php
@@ -14,6 +14,7 @@
         //$subtotal=200;
         include_once 'connections.php';
         if (isset($_POST['save'])) {
+			
             $date = $_POST['date'];
             //$invoiceNumber = $_POST['invoiceNumber'];
             $accCode = $_POST['accCode'];
@@ -21,12 +22,144 @@
             $itemNumber = $_POST['itemNumber'];
             $stockCode = $_POST['stockCode'];
             $quantitySold = $_POST['quantitySold'];
-            $unitCost = $_POST['unitCost'];
-            $unitSell = $_POST['unitSell'];
+            //$unitCost = $_POST['unitCost'];
+            //$unitSell = $_POST['unitSell'];
             $discount = $_POST['discount'];
-            $subTotal = $quantitySold * $unitSell - $discount;
+            //$subTotal = $quantitySold * $unitSell - $discount;
+			
+			$retq = "SELECT * FROM Stock WHERE stockCode='$stockCode'";
+			$res=$conn->query($retq);
+			//$resString = serialize($res) ;
+			//$resInt = (int)$resString;
+			//echo $resInt;
+			foreach($res as $rows)
+			{	
+				$unitCostInt = (double) $rows['unitCost'];
+				$unitSellInt = (double) $rows['unitSell'];
+	
+			}
 
-            $insertq = "INSERT INTO InvoiceDetail (date, accCode, name, itemNumber, stockCode, quantitySold, unitCost, unitSell, discount, subtotal) VALUES('$date','$accCode', '$name', '$itemNumber', '$stockCode', '$quantitySold', '$unitCost', '$unitSell', '$discount', '$subTotal')";
+			$subTotal = $quantitySold * $unitSellInt - $discount;
+			
+			$insertq = "INSERT INTO InvoiceDetail (date, accCode, name, itemNumber, stockCode, quantitySold, unitCost, unitSell, discount, subtotal) VALUES('$date','$accCode', '$name', '$itemNumber', '$stockCode', '$quantitySold', '$unitCostInt', '$unitSellInt', '$discount', '$subTotal')";
+			$result = $conn->query($insertq);
+			
+			//Reduce stock items by the number of items sold. Assumption is we can't sell something we don't have in stock so quantity will never be negative in Stock table.
+			
+			$deleteStockItem = "UPDATE `Stock` SET `quantity` = Stock.quantity-$quantitySold WHERE `Stock`.`stockCode` = '$stockCode'";
+			
+		 	$conn->query($deleteStockItem);
+
+            $retrieveLastRecord = "SELECT * FROM InvoiceDetail ORDER BY invoiceNumber DESC LIMIT 1";
+            $lastRecord = $conn->query($retrieveLastRecord);
+			
+			
+			
+			$updateStockMaster = "UPDATE StockMaster SET quantitySold=StockMaster.quantitySold+$quantitySold where stockCode='$stockCode'"; 
+			$res=$conn->query($updateStockMaster);
+			
+			
+			foreach ($lastRecord as $row) 
+				{
+					$invoiceNumber = $row['invoiceNumber'];
+					$unCost = $row['unitCost'];
+					$unSell = $row['unitSell'];
+				}
+			
+			if($result===TRUE)
+			{
+				echo "The following record has been added successfully";
+            ?> <form action="Invoice_Detail_Input.php" method="post">
+        <div class="container-fluid bg-info">
+            <h3 class="pageCenter"> Invoice Detail</h3>
+            <br>
+            <div class="row">
+                <div class="col-lg-2"> Date</div>
+                <div class="col-lg-1"> Invoice Number</div>
+                <div class="col-lg-1"> Account Code</div>
+                <div class="col-lg-1"> Name & Surname</div>
+                <div class="col-lg-1"> Item Number</div>
+                <div class="col-lg-1"> Stock Code</div>
+                <div class="col-lg-1"> Quantity Sold</div>
+                <div class="col-lg-1"> Unit Cost</div>
+                <div class="col-lg-1"> Unit Sell</div>
+                <div class="col-lg-1 "> Discount</div>
+				<div class="col-lg-1 "> Sub Total</div>
+
+            </div>
+            <div class="row">
+                <div class="col-lg-2"><input type="date" name="date" id="date" class="form-control" required value="<?php echo $date;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="invoiceNumber" id="invoiceNumber" class="form-control" readonly value="<?php echo $invoiceNumber;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="accCode" id="accCode" class="form-control" required value="<?php echo $row['accCode'];?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="name" id="name" class="form-control" required value="<?php echo $name;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="itemNumber" id="itemNumber" class="form-control" required value="<?php echo $itemNumber;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="stockCode" id="stockCode" class="form-control" required value="<?php echo $stockCode;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="quantitySold" id="quantitySold" class="form-control" required value="<?php echo $quantitySold;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="unitCost" id="unCost" class="form-control" disabled value="<?php echo $unitCostInt;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="unitSell" id="unitSell" class="form-control" disabled value="<?php echo $unitSellInt;?>"/>
+                </div>
+                <div class="col-lg-1"><input type="text" name="discount" id="discount" class="form-control" required value="<?php echo $discount;?>"/>
+                </div>
+				<div class="col-lg-1"><input type="text" name="subTotal" id="subTotal" class="form-control" required value="<?php echo $subTotal;?>"/>
+                </div>
+
+            </div>
+            <br />
+
+            <div class="row">
+                <div class="col-lg-4">
+                    
+                </div>
+                <div class=" col-lg-4">
+                    <a href="InvoiceDetail.php"><button class="btn btn-info btn-sm form-control" id="btic" name="btic">Back To Invoice Creation</button></a>
+                </div>
+
+                <div class="col-lg-4">
+                    
+                </div>
+
+            </div>
+            <br />
+        </div>
+    </form>
+    <br>
+    <br>
+    <form action="Invoice_Detail_Input.php" method="post">
+        <div class="row">
+            <div class="col-lg-3">
+                <button class="btn btn-info btn-sm form-control" id="search" name="search">Search</button>
+            </div>
+            <div class="col-lg-3">
+                <button class="btn btn-info btn-sm form-control" id="open" name="open">Open</button>
+            </div>
+            <div class="col-lg-3">
+                <button class="btn btn-info btn-sm form-control" id="delete" name="delete">Delete</button>
+            </div>
+        </div>
+    </form>
+
+	 <?php
+			}
+			
+			else{
+				echo "Ooops! ".$conn->error;
+			}
+		
+			
+			
+			
+			
+			/*
+			
+			$insertq = "INSERT INTO InvoiceDetail (date, accCode, name, itemNumber, stockCode, quantitySold, unitCost, unitSell, discount, subtotal) VALUES('$date','$accCode', '$name', '$itemNumber', '$stockCode', '$quantitySold', '$unitCost', '$unitSell', '$discount', '$subTotal')";
 			$deleteq = "DELETE FROM Stock WHERE stock.stockCode = '$stockCode'";  
 				$conn->query($deleteq);
 			$result = $conn->query($insertq);
@@ -79,7 +212,16 @@
                 echo "Error adding record(s)!: " . $conn->error;
             }
             echo "<br>";
-        } elseif (isset($_POST['nextItem'])) {
+			
+			*/
+        } 
+	 
+	 elseif(isset($_POST['btic']))
+			{
+				header("Location: InvoiceDetail.php");
+			}
+	 
+	 elseif (isset($_POST['nextItem'])) {
             $date = $_POST['date'];
             //$invoiceNumber = $_POST['invoiceNumber'];
             $accCode = $_POST['accCode'];
@@ -371,25 +513,6 @@ $id = $_REQUEST['id'];
     </form>
 
 
-
-
-
-
-
-    <script src="bootstrap/js/jquery.js" type="text/javascript"></script>
-    <script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
-    <script>
-        function displayTotal() {
-            document.getElementById("subTotal").innerHTML
-        }
-    </script>
-    <script>
-        function refreshPage() {
-            window.location.reload();
-        }
-    </script>
-    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
-	 
 	<?php 
 	 }
 	 
@@ -419,6 +542,10 @@ $id = $_REQUEST['id'];
 $conn->close();
  
 ?>
+	 
+	 <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+	 <script src="bootstrap/js/jquery.js" type="text/javascript"></script>
+    <script src="bootstrap/js/bootstrap.js" type="text/javascript"></script>
 
  </body>
 
